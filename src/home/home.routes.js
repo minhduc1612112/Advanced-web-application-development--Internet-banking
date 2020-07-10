7f1eb94e7328f9b327f984ca4765f55ce1b90865
minhduc1612112 <50917676+minhduc1612112@users.noreply.github.com>
2020-07-10 15:08:31 +0700

    Update home.routes.js

--- src/home/home.routes.js
+++ src/home/home.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.send('APP IS');
+    res.send('APP IS RUNNING');
 });
 
 module.exports = router;
